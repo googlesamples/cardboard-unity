39acabe5a7619fc071522b799b92c3ea4870fe48
zulman <zulman@bk.ru>
2015-10-07 23:43:40 +0300

    unity's Screen.dpi (since unity 5.1.3p2 changed to non physical screen density) changed to hardware dpi values

--- Cardboard/Scripts/CardboardUILayer.cs
+++ Cardboard/Scripts/CardboardUILayer.cs
@@ -57,8 +57,8 @@ public class CardboardUILayer {
   private Matrix4x4 xfm;
 
   private void ComputeMatrix() {
-    centerWidthPx = kCenterLineThicknessDp / 160.0f * Screen.dpi / 2;
-    buttonWidthPx = kButtonWidthDp / 160.0f * Screen.dpi / 2;
+    centerWidthPx = kCenterLineThicknessDp / 160.0f * DisplayMetricsAndroid.XDPI / 2;
+    buttonWidthPx = kButtonWidthDp / 160.0f * DisplayMetricsAndroid.YDPI / 2;
     xScale = buttonWidthPx / Screen.width;
     yScale = buttonWidthPx / Screen.height;
     xfm = Matrix4x4.TRS(new Vector3(0.5f, yScale, 0), Quaternion.identity,
